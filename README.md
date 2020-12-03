e9f17cb19b689fb933f2c43178830111bd76c4e8
cjsmith33 <45012535+cjsmith33@users.noreply.github.com>
2020-12-03 13:24:56 -0700

    Initial version of applicatoin.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Currency-Converter
+This project will implement a fully functioning currency converter. This is the third class in my certificate program.
---
+++ currency.py
@@ -0,0 +1,15 @@
+"""
+Module for currency exchange
+
+This module provides several string parsing functions to implement a simple
+currency exchange routine using an online currency service. The primary function
+in this module is exchange().
+
+Author: Charles Smith
+Date:   03 December 2020
+"""
+
+
+import introcs
+
+APIKEY = ''
