f1e988a542b9fd31b937cde787127d795b8e7574
cjsmith33 <45012535+cjsmith33@users.noreply.github.com>
2020-12-03 21:00:40 -0700

    Added new files testcurrency and exchangeit

---
+++ exchangeit.py
@@ -0,0 +1,11 @@
+"""
+User interface for module currency
+
+When run as a script, this module prompts the user for two currencies and amount.
+It prints out the result of converting the first currency to the second.
+
+Author: Charles Smith
+Date:   03 December 2020
+"""
+
+import currency
---
+++ testcurrency.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for module currency
+
+When run as a script, this module invokes several procedures that test
+the various functions in the module currency.
+
+Author: Charles Smith
+Date:   03 December 2020
+"""
+
+import introcs
+import currency
+
+print("All tests completed successfully.")
